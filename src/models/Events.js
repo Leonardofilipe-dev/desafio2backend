b83dabc00a065f1447cdf08acfc432b339368d47
Leonardo-dev <leonardo.filipe@yahoo.com>
2023-04-09 16:21:19 -0300

    add events

---
+++ src/models/Events.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose"
+
+const EventsSchema = new mongoose.Schema({
+    
+    dateEvents:{ 
+        type: Date,
+        trim:true,
+        required:true
+    
+    },
+    titleEvents:{
+        type:String,
+        trim: true
+    },
+    attractions:{
+        type: String
+    }, 
+    description:{
+        type:String
+    },
+    capacity:{
+        type: Number
+    }
+
+})
+
+const Events = mongoose.model("events", EventsSchema)
+
+
+export default Events
